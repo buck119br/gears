505ca4f8476692359e8f8051f51078ee73f7ed03
刘春旭 <lcx061125@163.com>
2022-06-15 22:31:43 +0800

    [core][gtype][watermark]: Watermark

---
+++ pkg/gears/core/gtype/watermark/watermark.go
@@ -0,0 +1,41 @@
+package watermark
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/buck119br/gears/pkg/gears/core/gtype/common"
+	"github.com/buck119br/gears/pkg/gears/core/gtype/element"
+)
+
+type Watermark interface {
+	fmt.Stringer
+	element.Element
+}
+
+func NewWatermark(i time.Time) Watermark {
+	w := &watermark{
+		i: i,
+	}
+	return w
+}
+
+type watermark struct {
+	i time.Time
+}
+
+func (wm *watermark) String() string {
+	return fmt.Sprintf("Watermark: [%s]", common.AnyToString(wm.i))
+}
+
+func (wm *watermark) Type() element.Type {
+	return element.Watermark
+}
+
+func (wm *watermark) Key() common.Key {
+	return nil
+}
+
+func (wm *watermark) Timestamp() time.Time {
+	return wm.i
+}
