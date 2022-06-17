0ddf090e499bfd2f9a2ff6e3b69fbf935b968035
刘春旭 <lcx061125@163.com>
2022-06-17 15:46:41 +0800

    [core][gtype][common]: change Value.Value() from []byte to any

--- pkg/gears/core/gtype/common/value.go
+++ pkg/gears/core/gtype/common/value.go
@@ -2,61 +2,59 @@ package common
 
 import (
 	"fmt"
-	"strconv"
-	"time"
 )
 
 type Value interface {
 	fmt.Stringer
 
-	Value() []byte
-	As(Type) interface{}
+	Value() any
+	// As(Type) interface{}
 }
 
-func NewValue(v []byte) Value {
-	val := new(value)
-	val.v = v
-
+func NewValue(v any) Value {
+	val := &value{
+		v: v,
+	}
 	return val
 }
 
 type value struct {
-	v []byte
+	v any
 }
 
 func (v *value) String() string {
-	return BytesToString(v.v)
+	return AnyToString(v.v)
 }
 
-func (v *value) Value() []byte {
+func (v *value) Value() any {
 	return v.v
 }
 
-func (v *value) As(t Type) interface{} {
-	var x interface{}
-	var err error
-	switch t {
-	case Bool:
-		x, err = strconv.ParseBool(BytesToString(v.v))
-	case Integer:
-		x, err = strconv.ParseInt(BytesToString(v.v), 10, 64)
-	case Unsigned:
-		x, err = strconv.ParseUint(BytesToString(v.v), 10, 64)
-	case Float:
-		x, err = strconv.ParseFloat(BytesToString(v.v), 64)
-	case String:
-		x, err = BytesToString(v.v), nil
-	case Binary:
-		x, err = v.v, nil
-	case Timestamp:
-		x, err = time.Parse(time.RFC3339Nano, BytesToString(v.v))
-	default:
-		x, err = nil, fmt.Errorf("invalid column type: [%d]", t)
-	}
-
-	if err != nil {
-		panic(fmt.Errorf("cannot parse value: [%s] as: [%s]", BytesToString(v.v), t))
-	}
-
-	return x
-}
+// func (v *value) As(t Type) interface{} {
+// 	var x interface{}
+// 	var err error
+// 	switch t {
+// 	case Bool:
+// 		x, err = strconv.ParseBool(BytesToString(v.v))
+// 	case Integer:
+// 		x, err = strconv.ParseInt(BytesToString(v.v), 10, 64)
+// 	case Unsigned:
+// 		x, err = strconv.ParseUint(BytesToString(v.v), 10, 64)
+// 	case Float:
+// 		x, err = strconv.ParseFloat(BytesToString(v.v), 64)
+// 	case String:
+// 		x, err = BytesToString(v.v), nil
+// 	case Binary:
+// 		x, err = v.v, nil
+// 	case Timestamp:
+// 		x, err = time.Parse(time.RFC3339Nano, BytesToString(v.v))
+// 	default:
+// 		x, err = nil, fmt.Errorf("invalid column type: [%d]", t)
+// 	}
+//
+// 	if err != nil {
+// 		panic(fmt.Errorf("cannot parse value: [%s] as: [%s]", BytesToString(v.v), t))
+// 	}
+//
+// 	return x
+// }
