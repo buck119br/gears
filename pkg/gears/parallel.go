bd94b9f9cb67265251704a527cabe28bf6757167
刘春旭 <lcx061125@163.com>
2022-06-21 13:45:26 +0800

    [API]: Parallel

---
+++ pkg/gears/parallel.go
@@ -0,0 +1,31 @@
+package gears
+
+import (
+	"github.com/buck119br/gears/pkg/gears/core/gfunc"
+	"github.com/buck119br/gears/pkg/gears/core/graph"
+	"github.com/buck119br/gears/pkg/gears/transform/parallel"
+)
+
+func Parallel(df Dataflow, o parallel.Option, f DoFunction, inputs []Dataset, args ...interface{}) []Dataset {
+	// edge
+	fn := gfunc.NewFunction(f)
+	n := df.Graph().AddNode(graph.Parallel, fn, args...).WithConcurrency(o.Concurrency())
+	// inbounds
+	for _, input := range inputs {
+		ib := graph.NewInbound(input.Edge())
+		n.AddIn(ib)
+	}
+	// outbounds
+	outputDs := make([]Dataset, 0, o.NumOutputs())
+	for i := 0; i < o.NumOutputs(); i++ {
+		output := df.Graph().AddEdge()
+
+		ob := graph.NewOutbound(output)
+		n.AddOut(ob)
+
+		outputD := NewDataset(output)
+		outputDs = append(outputDs, outputD)
+	}
+
+	return outputDs
+}
